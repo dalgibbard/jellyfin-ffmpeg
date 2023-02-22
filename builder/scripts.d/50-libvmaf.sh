70699d8ce67803067819f50410b08101c4d098ec
Darren Gibbard <dalgibbard@gmail.com>
2023-02-22 12:45:40 +0000

    Enable Libvmaf in static binary builds

---
+++ builder/scripts.d/50-libvmaf.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+SCRIPT_REPO="https://github.com/Netflix/vmaf"
+#SCRIPT_COMMIT="b9150a2301e75d280d6d885ae1a386cf45bb4c2c"
+SCRIPT_COMMIT="e81d12e75629f6f0730077bfa7e28764c41b2907"
+
+ffbuild_enabled() {
+    return 0
+}
+
+ffbuild_dockerbuild() {
+    git-mini-clone "$SCRIPT_REPO" "$SCRIPT_COMMIT" vmaf
+    cd vmaf/libvmaf
+
+    mkdir build && cd build
+
+    local myconf=(
+        --prefix="$FFBUILD_PREFIX"
+        --buildtype=release
+        --default-library=static
+	-Denable_float=true
+	-Denable_tests=false
+	-Denable_docs=false
+	-Denable_avx512=true
+	-Dbuilt_in_models=true
+        -Denable_cuda=false
+        -Denable_nvtx=false
+    )
+
+    if [[ $TARGET == win* || $TARGET == linux* ]]; then
+        myconf+=(
+            --cross-file=/cross.meson
+        )
+    else
+        echo "Unknown target"
+        return -1
+    fi
+
+    meson "${myconf[@]}" ..
+    ninja -j$(nproc)
+    ninja install
+
+    sed -i 's/Cflags:/Cflags: -DVMAF_LIB_STATIC/' "$FFBUILD_PREFIX"/lib/pkgconfig/libvmaf.pc
+}
+
+ffbuild_configure() {
+    echo --enable-libvmaf
+}
+
+ffbuild_unconfigure() {
+    echo --disable-libvmaf
+}
